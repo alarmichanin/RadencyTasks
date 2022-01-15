f2314deb1d8eb75c12900c93d69b22ef3647f887
ALARMich <55944078+alarmichanin@users.noreply.github.com>
2022-01-15 14:56:53 +0200

    header component with style

---
+++ task2/src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <div className="Header">
+            <div className="col">Name</div>
+            <div className="col">Created</div>
+            <div className="col">Category</div>
+            <div className="col">Content</div>
+            <div className="col">Dates</div>
+            <div className="col">icons</div>
+        </div>
+    );
+}
+
+export default Header;
