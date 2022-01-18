e8cfa960ab31b89d98c0a8ad47f042516e396d49
ALARMich <55944078+alarmichanin@users.noreply.github.com>
2022-01-18 12:52:40 +0200

    added validations + add function

--- task2/src/components/addForm/AddForm.js
+++ task2/src/components/addForm/AddForm.js
@@ -1,37 +1,44 @@
 import { useState } from "react";
 import { addTask } from "../../redux/actions";
 import { v1 as uuid } from 'uuid';
+import { useDispatch } from "react-redux";
+import { category, isNotEmpty } from './typesConst';
 const AddForm = () => {
     let [name, setName] = useState();
     let [type, setType] = useState();
     let [desc, setDesc] = useState();
+    let dispatch = useDispatch();
     return (
         <div className="addForm">
-            <label for="name">Name of point:</label>
+            <label htmlFor="name">Name of point:</label>
             <input onChange={(e) => setName(e.target.value)} value={name} type="text" id="name"></input>
-            <label for="typeTask">Type of your point:</label>
+            <label htmlFor="typeTask">Type of your point:</label>
             <select onChange={(e) => setType(e.target.value)} value={type} name="typeTask">
-                <option value="task">Task</option>
+                <option selected disabled value="">Choose Tagging</option>
+                <option value="taskType">Task</option>
                 <option value="randomThought">Random Thought</option>
                 <option value="idea">Idea</option>
             </select>
-            <label for="desc">Description of point:</label>
+            <label htmlFor="desc">Description of point:</label>
             <input onChange={(e) => setDesc(e.target.value)} value={desc} type="text" id="desc"></input>
             <button
                 onClick={() => {
-                    addTask(
-                        {
-                            id: uuid(),
-                            name: name,
-                            created: "October 14 2021",
-                            category: type,
-                            content: desc,
-                            dates: " "
-                        }
-                    )
-                    setName('')
-                    setType('')
-                    setDesc('')
+                    if ((isNotEmpty(name) || isNotEmpty(desc)) && type) {
+                        let date = new Date();
+                        dispatch(addTask(
+                            {
+                                id: uuid(),
+                                name: name,
+                                created: date.toLocaleString('en-EN', { month: 'long' }) + " " + date.getDate() + " " + date.getFullYear(),
+                                category: category[type],
+                                content: desc,
+                                dates: " "
+                            }
+                        ))
+                        setName('')
+                        setType('')
+                        setDesc('')
+                    }
                 }}
             >ADD TASK</button>
         </div>
